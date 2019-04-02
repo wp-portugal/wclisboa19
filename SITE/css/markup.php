291dbd2076618ac5df5d1e62bbdad0b8363df5a6
Helder Vilela <dev@heldervilela.com>
2019-04-02 01:52:49 +0000

    Home

---
+++ SITE/css/markup.php
@@ -0,0 +1,37 @@
+<div class="footer-sponsors">
+
+	<div class="footer-sponsors__title">
+		<h6>Sponsors</h6>
+		<h2>Obrigado a todos os <strong>patrocinadores</strong>.</h2>
+		<p>Sem o apoio destas entidades não seria possível realizar um evento com esta qualidade e com o valor dos bilhetes mais acessível de todos.</p>
+	</div>
+
+	<div class="footer-sponsors__container footer-sponsors__container--diamante">
+		<h4>Diamante</h4>
+		<div>
+			<a href="#"><img src="http://wclisboa19.local/media/diamante-01.png"/></a>
+			<a href="#"><img src="http://wclisboa19.local/media/diamante-02.png"/></a>
+			<a href="#"><img src="http://wclisboa19.local/media/diamante-03.png"/></a>
+		</div>
+	</div>
+
+	<div class="footer-sponsors__container footer-sponsors__container--ouro">
+		<h4>Ouro</h4>
+		<div>
+			<a href="#"><img src="http://wclisboa19.local/media/diamante-01.png"/></a>
+			<a href="#"><img src="http://wclisboa19.local/media/diamante-01.png"/></a>
+			<a href="#"><img src="http://wclisboa19.local/media/diamante-01.png"/></a>
+			<a href="#"><img src="http://wclisboa19.local/media/diamante-01.png"/></a>
+		</div>
+	</div>
+
+	<div class="footer-sponsors__container footer-sponsors__container--prata">
+		<h4>Prata</h4>
+		<div>
+			<a href="#"><img src="http://wclisboa19.local/media/diamante-01.png"/></a>
+			<a href="#"><img src="http://wclisboa19.local/media/diamante-01.png"/></a>
+			<a href="#"><img src="http://wclisboa19.local/media/diamante-01.png"/></a>
+		</div>
+	</div>
+
+</div>
